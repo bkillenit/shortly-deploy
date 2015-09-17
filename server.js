c6d09426955ae7193d9fe5df980e1a49502efef7
Eugene Lee <colemakdvorak@gmail.com>
2015-09-17 01:30:42 -0700

    Changed connection point of server

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var app = require('./server-config.js');
+var mongoose = require('mongoose');
 
 var port = process.env.PORT || 4568;
+
+// dunno what config is yet
+mongoose.connect(config.url);
 
 app.listen(port);
 
